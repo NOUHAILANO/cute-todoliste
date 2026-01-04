157a91871fd04304f1f1cb5546df2a2e61ae41d8
nouhaila <nouhailaboutrique@gmail.gom>
2026-01-04 18:36:54 +0100

    add startedit and saveedit functions

--- src/todoliste.js
+++ src/todoliste.js
@@ -2,6 +2,8 @@ import { useState,useEffect } from "react";
 function Todoliste(){
   const [tasks,settasks]=useState([])
   const [newtask,setnewtask]=useState('')
+  const [editid,seteditid]=useState(null)
+  const [newtitle,setnewtitle]=useState('')
   useEffect(()=>{
     fetch('http://localhost:3001/tasks')
     .then(res=>res.json())
@@ -21,12 +23,29 @@ const addtask=()=>{
   .then(task=>{settasks([...tasks,task]);
 setnewtask('')});
 }
+const startedit=(task)=>{
+  seteditid(task.id)
+  setnewtitle(task.title)
+}
+const saveedit=()=>{
+  fetch(`http://localhost:3001/tasks/${editid}`,{
+    method:'PUT',
+  headers:{'Content-Type':'application/json'},
+body:JSON.stringify({id:editid,title:newtitle})
+}
+)
+  .then(res=>res.json())
+  .then(updated=>{settasks(tasks.map((t)=>t.id===updated.id?updated:t));
+  seteditid(null);
+  setnewtitle("");})
+}
+
+
+
+
 
 return(
-  <div>
-    <input type="text" value={newtask} /><button onClick={addtask}>add</button>
-    {tasks.map((t)=>{return <div key={t.id}>{t.title}</div>})}
-  </div>
+  <div></div>
 )
   
 }export default Todoliste;
